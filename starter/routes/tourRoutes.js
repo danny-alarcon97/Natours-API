dc4fbe9a404de54c6e954e1521dbcf9032bba815
daniel <danny.alarcon1997@gmail.com>
2022-11-17 16:58:31 -0500

    param middleware

--- starter/routes/tourRoutes.js
+++ starter/routes/tourRoutes.js
@@ -4,6 +4,8 @@ const tourController = require('./../controllers/tourController');
 // ROUTES
 const router = express.Router();
 
+router.param('id', tourController.checkID);
+
 router
   .route('/')
   .get(tourController.getAllTours)
